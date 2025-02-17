e354d9109f7802d5bfab2c4443ed66a0f83717a8
MaxPress <maxpress96@ukr.net>
2025-02-17 21:29:30 +0200

    add js script

--- js/task-2.js
+++ js/task-2.js
@@ -1,12 +1,58 @@
-function getShippingMessage(country, price, deliveryFee) {
+const getUsersWithFriend = (users, friendName) => users.filter(user => user.friends.includes(friendName) > 0);
 
-    const totalPrice = price + deliveryFee;
+const allUsers = [
+  {
+    name: "Moore Hensley",
+    friends: ["Sharron Pace"]
+  },
+  {
+    name: "Sharlene Bush",
+    friends: ["Briana Decker", "Sharron Pace"]
+  },
+  {
+    name: "Ross Vazquez",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
+  },
+  {
+    name: "Elma Head",
+    friends: ["Goldie Gentry", "Aisha Tran"]
+  },
+  {
+    name: "Carey Barr",
+    friends: ["Jordan Sampson", "Eddie Strong"]
+  },
+  {
+    name: "Blackburn Dotson",
+    friends: ["Jacklyn Lucas", "Linda Chapman"]
+  },
+  {
+    name: "Sheree Anthony",
+    friends: ["Goldie Gentry", "Briana Decker"]
+  }
+];
 
-    return `Shipping to ${country} will cost ${totalPrice} credits`;
-}
+console.log(getUsersWithFriend(allUsers, "Briana Decker")); 
+// [
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"]
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ]
 
-console.log(getShippingMessage("Australia", 120, 50)); 
+console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
+// [
+//   {
+//     name: "Elma Head",
+//     friends: ["Goldie Gentry", "Aisha Tran"]
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ]
 
-console.log(getShippingMessage("Germany", 80, 20)); 
-
-console.log(getShippingMessage("Sweden", 100, 20)); 
+console.log(getUsersWithFriend(allUsers, "Adrian Cross" )); // []
